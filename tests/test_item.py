e034cfa75e94b2849a1ff1bc59b006faf7250eb1
Veronics S <korolova27@rambler.ru>
2023-10-11 00:17:03 +0300

    hw2

--- tests/test_item.py
+++ tests/test_item.py
@@ -9,3 +9,12 @@ def test_calculate_total_price():
 def test_apply_discount():
     Item.pay_rate = 0.8
     assert  Item.apply_discount(item1) == 8000
+
+def test_string_to_number():
+    assert  Item.string_to_number("5.5") == 5
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('file_test.csv')
+    item2 = Item.all[0]
+    assert item2.name == 'Смартфон'
+
